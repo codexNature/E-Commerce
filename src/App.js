c64b98e474038141711a6a453d44619f3c8abefd
Olusola Jaiyeola <olusolajaiyeola@gmail.com>
2023-10-11 17:28:33 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
           <Route path='/cart' element={<Cart />} />
           <Route path='/login' element={<LoginSignup />} />
           
+          
       </Routes>
     </BrowserRouter>
       
