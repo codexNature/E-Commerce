1d6b50b5153e4a1f201ffaa79302fd3dfe81bb73
Olusola Jaiyeola <olusolajaiyeola@gmail.com>
2023-10-14 04:50:26 +0100

    men, womem,kids section

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,18 +1,19 @@
 import React, { createContext } from 'react';
-import all_product from "../Components/Assets/all_product";
+import all_product from '../Components/Assets/all_product'
+
 
 
 export const ShopContext = createContext(null);
 
-const ShopContextProvider = (props)=>{
+const ShopContextProvider = (props) => {
 
-  const contextValue = {all_product}
+    const contextValue = {all_product};
 
-  return (
-    <ShopContext.Provider value={contextValue}>
+    return (
+      <ShopContext.Provider value={contextValue}>
         {props.children}
-    </ShopContext.Provider>
-  )
+      </ShopContext.Provider>
+    )
 }
 
 export default ShopContextProvider;
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -8,24 +8,26 @@ const ShopCategory = (props) => {
   const {all_product} = useContext(ShopContext);
   return (
     <div className='shop-category'>
-      <img src={props.banner} alt=" " />
+      <img className='shopcategory-banner' src={props.banner} alt="" />
       <div className="shopcategory-indexSort">
         <p>
           <span>Showing 1-12</span> out of 36 products
         </p>
         <div className="shopcategory-sort">
-          Sort by <img src={dropdown_icon} alt="" />
-        </div>  
+          Sort by <img  src={dropdown_icon} alt="" />
+        </div>
       </div>
       <div className="shopcategory-products">
         {all_product.map((item,i)=>{
-          if (props.category===Item.category) {
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-          }
-          else{
-            return null;
-          }
+            if (props.category===item.category) {
+              return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+            } else {
+              return null;
+            }
         })}
+      </div>
+      <div className="shopcategory-loadmore">
+        Explore More
       </div>
     </div>
   )
